cc61e30aff91a6fa53f0c4c802a8cc5a8cb39b19
Sodum.Karthik <108868028+SodumKarthik@users.noreply.github.com>
2024-03-22 15:21:21 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("Weather.csv")
+print(data.head())
+
+
+print(data.describe())
+
+
+print(data.info())
+
+
+x=data["Rainfall"]
+y=data["Evaporation"]
+plt.plot(x,y)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
